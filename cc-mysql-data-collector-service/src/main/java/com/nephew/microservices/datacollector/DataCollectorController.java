a1b2f483cb11f1b290c219fdc9ff4e5afe6ea2ba
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-08-21 16:35:16 -0400

    Working on the data collector. Unfinished.

--- cc-mysql-data-collector-service/src/main/java/com/nephew/microservices/datacollector/DataCollectorController.java
+++ cc-mysql-data-collector-service/src/main/java/com/nephew/microservices/datacollector/DataCollectorController.java
@@ -1,5 +1,8 @@
 package com.nephew.microservices.datacollector;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,29 +22,41 @@ public class DataCollectorController {
 	@Autowired
 	private MySqlCrudProxy mySqlCrudProxy;
 	
+	@Autowired
+	private FxdsRootMapper mapper;
+	
 	@GetMapping("/test")
 	public String testing() {
 		return "Test was successful";
 	}
 	
+	// TODO Finish this, you need to fix the logic here. 
 	@GetMapping("/data-collector/base={base}&quote={quote}&start_date={startDate}&end_date={endDate}")
-	public FxdsRoot testingApiEndpoint(@PathVariable String base, @PathVariable String quote, @PathVariable String startDate, @PathVariable String endDate) {
-		logger.info("testingApiEndpoint called with {} to {} period {}-{}.", base, quote, startDate, endDate);
+	public FxdsRoot collectDataByDates(@PathVariable String base, @PathVariable String quote, @PathVariable LocalDate startDate, @PathVariable LocalDate endDate) {
+		logger.info("collectDataByDates called with {} to {} period {}-{}.", base, quote, startDate, endDate);
+		long daysBetween = ChronoUnit.DAYS.between(startDate, endDate); // TODO check scratch pad to see how to use this. Should simplify the logic a lot. 
 		
-		String jsonResponse = fxdsProxy.retrieveFxdsData(base, quote, startDate, endDate);
+		int dateCompare = startDate.compareTo(endDate);
+		String jsonResponse = null;
+		if (dateCompare < 0) { // startDate is before endDate (valid case)
+		    dateCompare = startDate.compareTo(endDate.minusDays(1)); // Check if provided dates are exactly one day apart. 
+			if(dateCompare == 0) { // If they are one day apart
+				jsonResponse = fxdsProxy.retrieveFxdsData(base, quote, startDate.toString(), endDate.toString());
+				mapper.readJson(jsonResponse);
+			} else { // else multiple api calls are required. 
+		    	
+		    }
+			
+		} else if (dateCompare > 0) { // startDate is after endDate (invalid case)
+			// TODO throw invalid date exception? 
+		} else { // startDate is equal to endDate (invalid case)
+			// TODO throw invalid date exception? 
+		}
 		
 		
-		try {
-            ObjectMapper objectMapper = new ObjectMapper(); 
-            FxdsRoot root = objectMapper.readValue(jsonResponse, FxdsRoot.class);
-            mySqlCrudProxy.saveCurrencyData(root);
-            logger.info("testingApiEndpoint called with {} to {} period {}-{} request fulfilled.", base, quote, startDate, endDate);
-            return root;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		logger.info("testingApiEndpoint called with {} to {} period {}-{} request not fullfilled. Something went wrong.", base, quote, startDate, endDate);
-		return null;
+		// String jsonResponse = fxdsProxy.retrieveFxdsData(base, quote, startDate.toString(), endDate.toString());
+		logger.info("collectDataByDates called with {} to {} period {}-{} request not fullfilled. Something went wrong.", base, quote, startDate, endDate);
+		return mapper.getResponseRoot();
 	}
 
 }
---
+++ cc-mysql-data-collector-service/src/main/java/com/nephew/microservices/datacollector/FxdsRootMapper.java
@@ -0,0 +1,43 @@
+package com.nephew.microservices.datacollector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class FxdsRootMapper {
+	
+	private ObjectMapper objectMapper;
+	private FxdsRoot responseRoot;
+
+	public FxdsRootMapper() {
+		super();
+		this.objectMapper = new ObjectMapper();
+		this.responseRoot = new FxdsRoot();
+	}
+
+	public void readJson(String jsonResponse) {
+		try {
+			FxdsRoot root = objectMapper.readValue(jsonResponse, FxdsRoot.class);
+			if(root != null) {
+				responseRoot.getResponse().add(root.getResponse().get(0));
+			}
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public FxdsRoot getResponseRoot() {
+		return responseRoot;
+	}
+	
+}
