674b4e661232cf1da40717709568949d91b94a51
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-08-29 15:21:13 -0400

    Still working on getting the currency data into the db. Running into feign timeout.

--- cc-entry-service/src/main/java/com/nephew/microservices/ccentryservice/EntryController.java
+++ cc-entry-service/src/main/java/com/nephew/microservices/ccentryservice/EntryController.java
@@ -20,10 +20,10 @@ public class EntryController {
 
 	@Autowired
 	private MySqlCrudProxy mySqlProxy;
-	
+
 	@Autowired
 	private DataValidatorProxy dataValidatorProxy;
-	
+
 	@Autowired
 	private DataCollectorProxy dataCollectorProxy;
 
@@ -32,30 +32,41 @@ public class EntryController {
 		Pair pair = mySqlProxy.findByBaseCurrencyAndQuoteCurrency("USD", "EUR");
 		return pair;
 	}
-	
+
 	@GetMapping("/validate/base={base}&quote={quote}&begin={beginDate}&end={endDate}")
-	public void validate(@PathVariable String base, @PathVariable String quote, @PathVariable LocalDate beginDate, @PathVariable LocalDate endDate) {
-		
+	public void validate(@PathVariable String base, @PathVariable String quote, @PathVariable LocalDate beginDate,
+			@PathVariable LocalDate endDate) {
+
 		Boolean pairIsValid = dataValidatorProxy.validateCurrencyPairByDateRange(base, quote);
-		if(!pairIsValid) {
+		if (!pairIsValid) {
 			logger.warn("The currency pair was not validated: {} {}", base, quote);
-		} else if(pairIsValid) {
+		} else if (pairIsValid) {
 			logger.info("The currency pair was validated: {} {}", base, quote);
 		}
-		
+
 		Boolean dateRangeIsValid = dataValidatorProxy.validateDates(beginDate, endDate);
-		if(!dateRangeIsValid) {
+		if (!dateRangeIsValid) {
 			logger.warn("The date range was not validated: {} {}", beginDate, endDate);
-		} else if(dateRangeIsValid) {
+		} else if (dateRangeIsValid) {
 			logger.info("The date range was validated: {} {}", beginDate, endDate);
 		}
 	}
-	
-	@GetMapping("/save-x-rates-3-years")
+
+	// @GetMapping("/save-x-rates-2-years")
 	public ArrayList<XRatesUsdData> collectAndSaveXRatesUsdBase() {
-		ArrayList<XRatesUsdData> xRatesList = dataCollectorProxy.getCurrencyDataBaseUsdByRange(LocalDate.now().toString(), LocalDate.of(2021, 1, 1).toString());
-		mySqlProxy.saveAllXRates(xRatesList);
-		return xRatesList;
+		for (int i = 1; i < 24; i++) {
+			LocalDate baseDate = LocalDate.now().minusMonths(i);
+			ArrayList<XRatesUsdData> xRatesList = dataCollectorProxy.getCurrencyDataBaseUsdByRange(baseDate.toString(),
+					baseDate.minusMonths(2).toString());
+			mySqlProxy.saveAllXRates(xRatesList);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 }
--- cc-mysql-data-collector-service/src/main/java/com/nephew/microservices/datacollector/webreader/WebReaderService.java
+++ cc-mysql-data-collector-service/src/main/java/com/nephew/microservices/datacollector/webreader/WebReaderService.java
@@ -4,15 +4,20 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nephew.common.entities.XRatesUsdData;
 import com.nephew.common.support.LocalDateComparator;
+import com.nephew.microservices.datacollector.DataCollectorController;
 
 
 @Service
 public class WebReaderService {
+	
+	private Logger logger = LoggerFactory.getLogger(WebReaderService.class);
 	
 	private LocalDate DATE_TODAY = LocalDate.now();
     private String WEBSITE_URL = "https://www.x-rates.com/historical/?from=USD&amount=1&date=";
@@ -37,10 +42,23 @@ public class WebReaderService {
 		WebReader serverWebReader = new WebReader();
 		localDateComparator = new LocalDateComparator(start, end);
 		for (LocalDate localDate : localDateComparator.getRangeArrayList()) {
+			sleepForASecond();
 			ArrayList<XRatesUsdData> currencyArrayList = serverWebReader.getPageAsArrayList(WEBSITE_URL + localDate);
+			if(currencyArrayList == null) {
+				logger.warn("serverWebReader failed to return an array list. Method was called with: {}", WEBSITE_URL + localDate);
+			}
+			logger.info("serverWebReader returned an array list. Method was called with: {}", WEBSITE_URL + localDate);
 			returnList.addAll(currencyArrayList);
 		}
 		return returnList;
+	}
+	
+	public void sleepForASecond() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
